999728d1b260210610fcb917bce97efc355b295b
Valeria <valeri.kiass.love@gmail.com>
2025-05-12 14:50:19 +0300

    feat: блокировка всей формы при отправке комментария

--- src/pages/offer-screen/components/reviews-form.tsx
+++ src/pages/offer-screen/components/reviews-form.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch } from '../../../store';
 import { useParams } from 'react-router-dom';
 import { sendComment } from '../../../store/api-actions';
 import { toast } from 'react-toastify';
+import styles from './reviews-form.module.css';
 import RatingStars from './rating-stars';
 
 function ReviewsForm() {
@@ -53,26 +54,28 @@ function ReviewsForm() {
         handleSubmit(evt);
       }}
     >
-      <label className="reviews__label form__label" htmlFor="review">Your review</label>
-      <div className="reviews__rating-form form__rating">
-        <RatingStars rating={review.rating} onRatingChange={handleRatingChange} />
-      </div>
-      <textarea
-        className="reviews__textarea form__textarea"
-        id="review"
-        name="review"
-        placeholder="Tell how was your stay, what you like and what can be improved"
-        value={review.comment}
-        onChange={handleCommentChange}
-      />
-      <div className="reviews__button-wrapper">
-        <p className="reviews__help">
-          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-        </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled={isSending || review.comment.length < 50 || review.comment.length > 300 || review.rating === 0}>
-          Submit
-        </button>
-      </div>
+      <fieldset disabled={isSending} className={styles.formFieldset}>
+        <label className="reviews__label form__label" htmlFor="review">Your review</label>
+        <div className="reviews__rating-form form__rating">
+          <RatingStars rating={review.rating} onRatingChange={handleRatingChange} />
+        </div>
+        <textarea
+          className="reviews__textarea form__textarea"
+          id="review"
+          name="review"
+          placeholder="Tell how was your stay, what you like and what can be improved"
+          value={review.comment}
+          onChange={handleCommentChange}
+        />
+        <div className="reviews__button-wrapper">
+          <p className="reviews__help">
+            To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+          </p>
+          <button className="reviews__submit form__submit button" type="submit" disabled={ review.comment.length < 50 || review.comment.length > 300 || review.rating === 0}>
+            Submit
+          </button>
+        </div>
+      </fieldset>
     </form>
   );
 }
